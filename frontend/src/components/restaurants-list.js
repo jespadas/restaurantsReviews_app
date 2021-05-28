ccaa8665e3475bfc6f4823beefe2a3f167ca239a
jespadas <julio.espadas@gmail.com>
2021-05-27 19:39:52 -0500

    components, axios and service donde

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function RestaurantsList() {
+    return (
+        <div className="App">
+            Hello world
+        </div>
+    );
+}
+
+export default RestaurantsList;
