769179185275f5bdaebc6e75270495a413cd65d0
jespadas <julio.espadas@gmail.com>
2021-05-27 22:03:47 -0500

    triying serveless with Realm on mongodb

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/restaurants",
+    baseURL: "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-review-app-askvp/service/restaurants/incoming_webhook",
     headers: {
         "Content-type": "application/json"
     }
-});
+})
