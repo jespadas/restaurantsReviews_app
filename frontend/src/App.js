4774f640d2213eb753042f80e3c7b881767848ae
jespadas <julio.espadas@gmail.com>
2021-05-29 19:16:27 -0500

    just logo added to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import AddReview from "./components/add-review";
 import Restaurant from "./components/restaurants";
 import RestaurantsList from "./components/restaurants-list";
 import Login from "./components/login";
+import logo from './logo.png';
 
 function App() {
   const [user, setUser] = React.useState(null);
@@ -22,7 +23,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
+          <img src={logo} alt="" style={{ height: "5%", width: "5%" }} />
         </a>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
